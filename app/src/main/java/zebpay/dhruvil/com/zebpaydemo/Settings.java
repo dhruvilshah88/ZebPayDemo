e1b1b77388f1f038b2716c3e95baf1408a3ddfe9
Dhruvil <dhruvilshah88@gmail.com>
2015-11-26 03:42:41 +0530

    v1.1

--- app/src/main/java/zebpay/dhruvil/com/zebpaydemo/Settings.java
+++ app/src/main/java/zebpay/dhruvil/com/zebpaydemo/Settings.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
@@ -20,6 +21,7 @@ public class Settings extends AppCompatActivity {
     Button buok;
     Switch notswitch;
     DiscreteSeekBar seekbar;
+    TextView currentvarience;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +37,23 @@ public class Settings extends AppCompatActivity {
         notswitch = (Switch) findViewById(R.id.notswitch);
         buok = (Button) findViewById(R.id.buok);
         seekbar = (DiscreteSeekBar) findViewById(R.id.seekbar);
+        currentvarience = (TextView) findViewById(R.id.currentvarience);
 
         final User user = ApplicationClass.getInstance().getsharedprefs();
+        currentvarience.append(user.getVariance() + "");
         seekbar.setProgress(user.getVariance());
         notswitch.setChecked(user.isNotify());
         buok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                User userok = new User();
-                userok.setNotify(notswitch.isChecked());
-                userok.setVariance(seekbar.getProgress());
-                ApplicationClass.getInstance().setsharedprefs(userok);
+
+                user.setNotify(notswitch.isChecked());
+                user.setVariance(seekbar.getProgress());
+                currentvarience.setText("Current variance is " + user.getVariance() + "");
+                ApplicationClass.getInstance().setsharedprefs(user);
                 Toast.makeText(Settings.this, "Updated", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
@@ -55,7 +61,7 @@ public class Settings extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
+        inflater.inflate(R.menu.setting, menu);
         return true;
     }
 
